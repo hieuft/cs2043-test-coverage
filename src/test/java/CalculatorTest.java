d135c22f71696af04df33bd24ab901f068f680c2
hieuft <work.hieuft@gmail.com>
2026-05-08 11:18:11 +0700

    test with coverage > 80

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -9,4 +9,16 @@ class CalculatorTest {
     assertEquals(5, calculator.add(2, 3), "2 + 3 = 5");
   }
 
+  @Test
+  void testDivide() {
+    assertEquals(2, calculator.divide(6, 3), "6 / 3 = 2");
+  }
+
+  @Test
+  void testDivideByZero() {
+    Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+      calculator.divide(5, 0);
+    });
+    assertEquals("Cannot be divided by 0", exception.getMessage());
+  }
 }
